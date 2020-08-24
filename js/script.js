1ebff6866d6b82aa7a94f443b20d93283825468e
nsikakowo <owonsikak@gmail.com>
2020-08-24 20:04:00 +0100

    mobile nav responsiveness

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const mobileBtn = document.querySelector(".mobile-nav-icon button");
+const mobileOverlay= document.querySelector("header nav");
+const mobileMenuContainer = document.querySelector(".mobile-nav-container");
+
+let menuBar ='<i class="fas fa-bars"></i>';
+let closeBtn = '<img src="./images/icon-close.svg" alt="close Button">';
+
+mobileBtn.addEventListener('click', function(){
+    mobileMenuContainer.style.display = "flex";
+    mobileBtn.innerHTML = closeBtn;
+})
+
+mobileOverlay.addEventListener('click', function(){
+    mobileMenuContainer.style.display = "none";
+    mobileBtn.innerHTML = menuBar;
+})
